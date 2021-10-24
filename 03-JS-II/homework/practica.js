3f7217da4eb4d1a4c4aeabebc1ec18d07dc7dbe2
ReneCadena <renecadenacontacto@gmail.com>
2021-10-24 17:30:59 -0300

    test

---
+++ 03-JS-II/homework/practica.js
@@ -0,0 +1,22 @@
+
+// var array = [4, 4, 1, 5, 10]
+
+// // var dato1 = array[0]
+
+// var num = 4
+
+// for (i = 0; i <= array.length-1 ; i++) {
+
+//     if(num === array[i]) {
+//         console.log(array[i])
+//     } else {
+//         console.log('No se encuentra')
+//     }
+
+// }
+
+// var i = 0
+// while (i<8) {
+//     numero + mu
+//     i++
+// }
